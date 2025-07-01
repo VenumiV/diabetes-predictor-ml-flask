f4a540343c712fafc72ee662b986304eaf0dedfb
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-07-01 19:10:00 +0530

    fixed some errors

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from src.routes.ui import ui_bp
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
     app.register_blueprint(ui_bp)
     return app
--- src/routes/ui.py
+++ src/routes/ui.py
@@ -5,17 +5,32 @@ from src.utils.preprocess import preprocess
 
 ui_bp = Blueprint('ui', __name__)
 
-model = joblib.load('../../models/FC110595_thathsara/diabetes_xgb_model.pkl')
+model = joblib.load('models/FC110595_thathsara/diabetes_xgb_model.pkl')
 
 @ui_bp.route('/')
 def index():
-    return render_template('../templates/index.html', prediction="")
+    return render_template('index.html', prediction="")
 
 @ui_bp.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
+    data = {
+        'Pregnancies': float(request.form['Pregnancies']),
+        'Glucose': float(request.form['Glucose']),
+        'BloodPressure': float(request.form['BloodPressure']),
+        'SkinThickness': float(request.form['SkinThickness']),
+        'Insulin': float(request.form['Insulin']),
+        'BMI': float(request.form['BMI']),
+        'DiabetesPedigreeFunction': float(request.form['DiabetesPedigreeFunction']),
+        'Age': float(request.form['Age'])
+    }
+    print(data)
     features = preprocess(data)
-    prediction = model.predict(np.array(features))
-    result = int(prediction[0])
+    features = np.array([features])
+    prediction = model.predict_proba(features)
+    print(prediction)
+    if prediction[0][1] > 0.5:
+        result = 1
+    else:
+        result = 0
     prediction = "Diabetes" if result == 1 else "No Diabetes"
-    return render_template('../templates/index.html', prediction=prediction)
+    return render_template('index.html', prediction=prediction)
